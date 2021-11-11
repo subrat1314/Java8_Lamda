df0245a1d7f08185909688d5e25fe62acdf71d4e
subratsahu1314 <subratsahu1314@gmail.com>
2021-11-11 12:51:41 +0530

    Adding 4th Question of Java 8 Lamda Assignment

---
+++ OddLength_Ques4.java
@@ -0,0 +1,24 @@
+package java8_Lamda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddLength_Ques4 {
+    public static void main(String[] args) {
+        List<String> Names=new ArrayList<>();
+        Names.add("Subrat");
+        Names.add("Saroj");
+        Names.add("Krishna");
+        Names.add("Kedar");
+        Names.add("Pravin");
+        
+        
+        Names.removeIf(s ->(s.length())%2!=0);
+
+        for (String n : Names) {
+            System.out.println(n);
+        }
+
+    }
+}
+
